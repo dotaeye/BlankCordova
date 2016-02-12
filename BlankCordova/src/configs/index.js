1af45e493575621f237d647cb6f44edefbfbac8a
navy235 <shenhaijunmail@163.com>
2016-02-12 18:13:19 +0800

    initial project

---
+++ BlankCordova/src/configs/index.js
@@ -0,0 +1,10 @@
+let clientConfig = {
+    host: 'http://www.dotaeye.com',
+    port: 3035,
+    apiRoot: '/api',
+    virtualPath: '',
+    authToken: 'userAuth',
+    product: false
+}
+
+export default clientConfig;
